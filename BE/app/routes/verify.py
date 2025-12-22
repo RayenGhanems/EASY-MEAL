217052773bb0fe945172fd371bbeccf9e462424d
mhdhajjar <mohamadhajjar112002@gmail.com>
2025-12-22 13:24:21 +0100

    handling errors

--- BE/app/routes/verify.py
+++ BE/app/routes/verify.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, Depends
 import base64
-from BE.app.chains.translate_to_ingredients_set_units import verifying_ingredients_chain
+from app.chains.translate_to_ingredients_set_units import verifying_ingredients_chain
 from app.core.security import get_current_user
 
 router = APIRouter(tags=["verify"])
