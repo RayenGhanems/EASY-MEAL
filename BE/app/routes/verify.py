34e9756a80de23e322046d288190666995447828
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2025-12-22 17:28:37 +0100

    a

--- BE/app/routes/verify.py
+++ BE/app/routes/verify.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, UploadFile, File, Depends
 import base64
-from app.chains.translate_to_ingredients_set_units import verifying_ingredients_chain
+from BE.app.chains.translate_to_ingredients_set_units import verifying_ingredients_chain
 from app.core.security import get_current_user
 
 router = APIRouter(tags=["verify"])
 
 @router.post("/verify")
-async def verify():
+async def verify(list :list):
+    user_id: int = Depends(get_current_user)
     return[]
--- BE/app/sql/sql_models.py
+++ BE/app/sql/sql_models.py
@@ -7,9 +7,9 @@ class Ingredient(SQLModel, table=True):
     ingredient_name: str = Field(nullable=False, index=True)
     measuring_unit: str = Field(nullable=False)
 
-class User(SQLModel, table=True):
-    __tablename__ = "users"
+class StoredIngredients(SQLModel, table=True):
+    __tablename__ = "ingredients_stored"
 
-    user_id: int = Field(primary_key=True, index=True)
-    username: str = Field(unique=True, index=True, max_length=255)
-    password: str = Field(max_length=255)
+    user_id: int = Field(foreign_key="user.user_id")
+    ingredient_id: int = Field(foreign_key="ingredients.ingredient_id")
+    amount: float
